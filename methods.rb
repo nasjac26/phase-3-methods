c486f0c7d1b513ebe114aeb2522894900b1f5458
Jacob <Jacobonasrallah@gmail.com>
2021-12-23 11:19:16 -0800

    commit tree c7321a1687f277043320adfb1248356377a3b2a2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+# def my_method (param = 30)
+#     puts "Caluculating your number plus 1"
+#     param + 1
+# end
+
+def greet_programmer(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def greet(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num1
+    if num1.class == Integer
+        return num1/2
+    else
+        nil
+    end
+end
